6358e1e1661ea6647746a42ea610205ddd790166
eliotourvieille <e.tourvieille@gmail.com>
2024-12-20 10:55:19 +0100

    Add favicon

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,4 +1,9 @@
 import UserHeader from "../../components/UserHeader";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "GameSquad - Dashboard",
+  description: "Votre plateforme de partage de jeux vidéo",
+};
 
 export default function UserLayout({
   children,
