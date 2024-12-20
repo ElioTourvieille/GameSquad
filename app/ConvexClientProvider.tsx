b6ae22de679e1d6c2bc0a74901f6f0082d9aaf60
eliotourvieille <e.tourvieille@gmail.com>
2024-12-20 13:20:37 +0100

    Update variable

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -5,7 +5,7 @@ import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
  
 const convex = new ConvexReactClient(
-  process.env.NEXT_PUBLIC_CONVEX_URL || "https://wonderful-meerkat-333.convex.dev"
+  process.env.NEXT_PUBLIC_CONVEX_URL!
 );
 
  
